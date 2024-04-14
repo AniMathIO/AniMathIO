e1f98233643b64578a8e4c9905a18e31d570c7d1
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-04-14 12:00:36 +0300

    feat(app): refactored styles

--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -1,18 +1,15 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: [
-    './renderer/pages/**/*.{js,ts,jsx,tsx}',
-    './renderer/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./renderer/pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      // use colors only specified
-      white: colors.white,
-      gray: colors.gray,
-      blue: colors.blue,
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
-    extend: {},
   },
   plugins: [],
-}
+};
