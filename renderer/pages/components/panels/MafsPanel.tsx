8c6e59f75d903c508507bdddb86db7701ecd5f8c
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-05-13 09:36:28 +0300

    feat(mafs): centered elements in the preview

--- renderer/pages/components/panels/MafsPanel.tsx
+++ renderer/pages/components/panels/MafsPanel.tsx
@@ -16,38 +16,38 @@ const generateMafsResources = (): MafsResourceType[] => {
     {
       index: 1,
       name: "Line",
-      children: <Line.Segment point1={[-2, -1]} point2={[2, 1]} />,
+      children: <Line.Segment point1={[-2, 1]} point2={[2, 3]} />,
       coordinates: false,
     },
     {
       index: 2,
       name: "Point",
-      children: <Point x={1} y={1} />,
+      children: <Point x={0} y={2.3} />,
       coordinates: false,
     },
     {
       index: 3,
       name: "Circle",
-      children: <Circle center={[0, 0]} radius={1} />,
+      children: <Circle center={[0, 2.3]} radius={1} />,
       coordinates: false,
     },
     {
       index: 4,
       name: "Polygon",
-      children: <Polygon points={[[0, 0], [1, 1], [1, -1]]} />,
+      children: <Polygon points={[[-1, 2], [1, 1.5], [1, 3]]} />,
       coordinates: false,
     },
     {
       index: 5,
       name: "Text",
-      children: <Text x={0} y={0} children="Hello, Mafs!" />,
+      children: <Text x={0} y={2} children="Hello, Mafs!" />,
       coordinates: false,
     },
     {
       index: 6,
       name: "LaTex",
       children: <LaTeX
-        at={[3, 2]}
+        at={[0, 2.3]}
         tex={String.raw`
       \begin{bmatrix} ${[1, 0]} \\ ${[0, 1]} \end{bmatrix}
     `} />,
@@ -71,7 +71,7 @@ export const MafsPanel = observer(() => {
       <div className="flex flex-col items-center justify-center">
         {mafsResources.map((resource) => (
           <MafsResource key={`mafs-${resource.index}`} index={resource.index} name={resource.name}>
-            <Mafs pan={true} width={250} height={300} viewBox={{ y: [-10, 5], x: [-5, 5] }}>
+            <Mafs pan={false} width={250} height={300} zoom={false}>
               {/* {resource.coordinates ? <Coordinates.Cartesian /> : null} */}
               {resource.coordinates ? resource.coordinateType === "cartesian" ? <Coordinates.Cartesian /> : <Coordinates.Polar /> : null}
               {resource.children}
