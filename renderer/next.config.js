4f3e8c5e2ddd47027ba4feb233cb855c94ef8451
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-05-17 03:23:54 +0300

    fix!: working on fixing windows build, had to downgrade nextjs

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   trailingSlash: true,
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   images: {
     unoptimized: true,
   },
