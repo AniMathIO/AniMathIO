a9a3c87f051f8bfa7186608c56cff594fcebf118
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-05-19 21:26:50 +0300

    feat: added new mafs elements

--- renderer/pages/components/panels/MafsPanel.tsx
+++ renderer/pages/components/panels/MafsPanel.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
-import { Mafs, Coordinates, Line, Point, Circle, Polygon, Text, LaTeX } from "mafs";
+import { Mafs, Coordinates, Line, Point, Circle, Polygon, Text, LaTeX, Ellipse, Plot, Vector, Transform } from "mafs";
 import MafsResource from "../entity/MafsResource";
 import MafsModal from "../partials/MafsModal";
 import { MafsResourceType } from "@/types";
@@ -10,36 +10,6 @@ const generateMafsResources = (): MafsResourceType[] => {
   const resources: MafsResourceType[] = [
     {
       index: 1,
-      name: "Line",
-      children: <Line.Segment point1={[-2, 1]} point2={[2, 3]} />,
-      coordinates: true,
-    },
-    {
-      index: 2,
-      name: "Point",
-      children: <Point x={0} y={2.3} />,
-      coordinates: true,
-    },
-    {
-      index: 3,
-      name: "Circle",
-      children: <Circle center={[0, 2.3]} radius={1} />,
-      coordinates: true,
-    },
-    {
-      index: 4,
-      name: "Polygon",
-      children: <Polygon points={[[-1, 2], [1, 1.5], [1, 3]]} />,
-      coordinates: true,
-    },
-    {
-      index: 5,
-      name: "Text",
-      children: <Text x={0} y={2} children="Hello, AniMathIO!" />,
-      coordinates: false,
-    },
-    {
-      index: 6,
       name: "LaTex",
       children: <LaTeX
         at={[0, 2.3]}
@@ -47,7 +17,55 @@ const generateMafsResources = (): MafsResourceType[] => {
       \begin{bmatrix} ${[1, 0]} \\ ${[0, 1]} \end{bmatrix}
     `} />,
       coordinates: false,
-    }
+    },
+    {
+      index: 2,
+      name: "Text",
+      children: <Text x={0} y={2.3} children="Hello, AniMathIO!" />,
+      coordinates: false,
+    },
+    {
+      index: 3,
+      name: "Point",
+      children: <Point x={0} y={2.3} />,
+      coordinates: true,
+    },
+    {
+      index: 4,
+      name: "Line",
+      children: <Line.Segment point1={[-2, 1]} point2={[2, 3]} />,
+      coordinates: true,
+    },
+    {
+      index: 5,
+      name: "Vector",
+      children: <Vector tail={[-1, 1]} tip={[2, 3]} />,
+      coordinates: true,
+    },
+    {
+      index: 6,
+      name: "Polygon",
+      children: <Polygon points={[[-1, 2], [1, 1.5], [1, 3]]} />,
+      coordinates: true,
+    },
+    {
+      index: 7,
+      name: "Plot",
+      children: <Plot.OfX y={(x) => Math.exp(x)} />,
+      coordinates: true,
+    },
+    {
+      index: 8,
+      name: "Circle",
+      children: <Circle center={[0, 2.3]} radius={1} />,
+      coordinates: true,
+    },
+    {
+      index: 9,
+      name: "Ellipse",
+      children: <Ellipse center={[0, 2.3]} radius={[2, 1]} />,
+      coordinates: true,
+    },
   ];
 
   return resources;
@@ -76,7 +94,7 @@ const MafsPanel = observer(() => {
       <div className="flex flex-col items-center justify-center">
         {mafsResources.map((resource) => (
           <MafsResource
-            key={`mafs-${resource.index}`}
+            key={`mafs - ${resource.index} `}
             name={resource.name}
             onAddResource={() => openModal(resource)}
           >
